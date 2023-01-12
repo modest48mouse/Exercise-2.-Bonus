6e795a67c4ee48a846a451f7ad75aef25b9d18fb
Alina test <modest48mouse@gmail.com>
2023-01-12 20:39:26 +0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
     public static void main(String[] args) {
 
         int amount = 100;
-        int payment = 1100;
+        int payment = 1009;
 
-        int bonus = (payment >= 1000) ? (payment / 100) : 0;
+        int bonus = (payment > 1000) ? (payment / 100) : 0;
         System.out.println(bonus);
 
         System.out.println("Итоговый счет: " + (amount + payment + bonus));
-        
+
     }
 }
